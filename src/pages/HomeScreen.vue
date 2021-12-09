cac6d7fee07e0d599b0d74e3c7aee8519e998335
thanhthien0706 <thanhthien24102002@gmail.com>
2021-12-09 23:44:05 +0700

    first commit

---
+++ src/pages/HomeScreen.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home_screen">
+      <h1>đây là trang home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "HomeScreen",
+    
+}
+</script>
+
+<style>
+
+</style>
